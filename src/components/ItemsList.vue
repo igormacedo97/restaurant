a1968e8a4f74459211727a52ba2f824d8032360a
igormacedo97 <igor.macedo097@gmail.com>
2022-01-23 18:10:02 -0300

    add loading and setTimeout in loading

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="items-list">
-    {{ selectedCategory }}
+    <Loading v-if="isLoading" />
     <Item v-for="item in itemsList" :key="item.id" :item="item" />
   </div>
 </template>
@@ -8,15 +8,18 @@
 <script>
 import axios from "axios";
 import Item from "./Item.vue";
+import Loading from "./Loading.vue";
 
 export default {
   name: "ItemsList",
   components: {
     Item,
+    Loading,
   },
   data() {
     return {
       itemsList: [],
+      isLoading: false,
     };
   },
   created() {},
@@ -29,11 +32,17 @@ export default {
   },
   methods: {
     getItemsList() {
-      axios
-        .get(`http://localhost:3000/${this.selectedCategory}`)
-        .then((response) => {
-          this.itemsList = response.data;
-        });
+      this.isLoading = true;
+      this.itemsList = [];
+
+      setTimeout(() => {
+        axios
+          .get(`http://localhost:3000/${this.selectedCategory}`)
+          .then((response) => {
+            this.itemsList = response.data;
+            this.isLoading = false;
+          });
+      }, 1500);
     },
   },
   watch: {
@@ -48,6 +57,7 @@ export default {
 .items-list {
   margin: 50px;
   display: flex;
+  width: 100%;
 
   @media @tablets {
     flex-wrap: wrap;
---
+++ src/components/Loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading"></div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+};
+</script>
+
+<style lang="less" scoped>
+.loading {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 3px solid #ffe4a1; // @light-yellow não funcionando no navegador
+  border-top-color: @yellow;
+  animation: spin 1s linear infinite;
+  margin: auto;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
